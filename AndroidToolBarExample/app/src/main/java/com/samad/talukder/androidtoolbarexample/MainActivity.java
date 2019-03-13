580374991c94e1c62b735db2766a26d6e8de1eda
samadtalukder <samadtalukdar6@gmail.com>
2019-03-13 13:22:25 +0600

    nav menu icon add,hide toolbar text,text color

--- AndroidToolBarExample/app/src/main/java/com/samad/talukder/androidtoolbarexample/MainActivity.java
+++ AndroidToolBarExample/app/src/main/java/com/samad/talukder/androidtoolbarexample/MainActivity.java
@@ -12,7 +12,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = findViewById(R.id.toolBar);
-        toolbar.setNavigationIcon(R.mipmap.ic_launcher);
+        // show title in toolbar
+        toolbar.setTitle(getString(R.string.app_name));
+        // show subtitle in toolbar
+        toolbar.setSubtitle(getString(R.string.app_name));
+        // show navigation icon in toolbar
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.nav_menu));
+        // show title text color in toolbar
+        // toolbar.setTitleTextColor(getResources().getColor(R.color.colorAccent));
+        // show subtitle text color in toolbar
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorAccent));
+
         setSupportActionBar(toolbar);
+        // hide toolbar text
+        // getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
